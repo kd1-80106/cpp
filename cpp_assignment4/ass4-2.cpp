f594e9fb997dd4e0dd333369de8b0d8c943b9728
shrilek <shrilekhshrikhande06@gmail.com>
2023-09-26 22:46:40 +0530

    ass4-2 and ass4-3

--- cpp_assignment4/ass4-2.cpp
+++ cpp_assignment4/ass4-2.cpp
@@ -1,4 +1,122 @@
 #include<iostream>
 using namespace std ;
 
+#define SIZE 5 
+
+
+
+class Stack
+{
+   private :
+
+   int top ;
+   int capacity ;
+   int *  arr ;
+
+  public :
+
+  Stack(int size = SIZE);  // ctor
+  
+  
+  ~Stack() ;            // dtor 
+
+  void push(int) ;
+
+  int pop();
+
+  int peek();
+
+  int size();
+
+  bool isFull();
+
+  bool isEmpty();
+
+  void print(){
+   for(int i = 0 ; i < SIZE ; i++ )
+   {
+      cout << this-> arr[i] << endl ;
+   }
+  }
+   
+
+
+};
+
+ Stack :: Stack(int size)
+ {
+    arr = new int[size];
+    capacity = size ;
+    top = -1 ;
+ }
+
+ Stack :: ~Stack()
+ {
+    delete[] arr;
+ }
+
+ bool Stack :: isFull()
+ {
+    return top == capacity - 1 ;
+ }
+
+ bool Stack :: isEmpty()
+ {
+    return top = -1 ;
+ }
+
+ int Stack :: peek()
+ {
+   if( !isEmpty() )
+      return arr[top] ;
+ }
+
+ void Stack :: push(int x)
+ { 
+    if( !isFull())
+      arr[++top] = x ;
+ }
+ int Stack :: pop()
+ {
+    if(isEmpty())
+    {
+        cout << "stack underflow " << endl ;
+        return 0 ;
+    }
+    else 
+    {
+        int popval = arr[top];
+        arr[top] = 0 ;
+        top-- ;
+        return popval ;
+    }
+ }
+ int Stack :: size()
+ {
+   return top++ ;
+ }
+
+  
+
+int main()
+{
+  
+    
+  Stack s1(5);
+
+  for(int i = 1 ; i< 6 ; i++)
+  {
+   s1.push(i) ;
+  }
+  for(int i = 1 ; i< 6 ; i++)
+  {
+   s1.peek() ;
+  }
+ 
+   s1.print() ;
+  
+
+    return  0 ;
+}
+
 
--- cpp_assignment4/ass4-3.cpp
+++ cpp_assignment4/ass4-3.cpp
@@ -3,7 +3,103 @@ using namespace  std ;
 
 class Matrix 
 { 
+  private: 
 
+  int row ;
+  int col ;
+
+  int** arr ;
+
+  public :
+
+  Matrix()
+  {
+    this-> row = 0 ;
+    this -> col = 0 ;
     
+  }
+
+  Matrix(int row ,int col)
+  {
+    this -> row = row ;
+    this -> col = col ;
+    arr = new int*[row] ;
+
+    for(int i = 0 ; i < row ; i++)
+    {
+        arr[i] = new int[col];
+    }
+  }
+  
+  void acceptArr()
+  {
+    cout <<" enter the matrix values " << endl ;
+    for(int i = 0 ; i< row ;i++)
+    {
+        for(int j = 0 ; j < col ; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+  }
+
+  void printArr()
+  {
+    cout << " the result = " << endl ;
+    for(int i = 0 ; i< col ;i++)
+    {  
+        cout << endl ;
+        for(int j = 0 ; j < col ; j++)
+        {
+            cout << arr[i][j] <<"\t" ;
+        }
+        cout << endl ;
+    }
+
+  }
+
+  void addArr(Matrix* m1 , Matrix* m2)
+  {
+     for(int i = 0 ; i < row ; i++)
+     {
+        for(int j = 0 ; j < col ; j++)
+        {
+         arr[i][j] = m1->arr[i][j] + m2 -> arr[i][j] ;
+        }
+     }
+  }
+
+  void subArr(Matrix * m1 , Matrix * m2)
+  {
+     for(int i = 0 ; i < row ; i++)
+     {
+        for(int j = 0 ; j < col ; j++)
+        {
+         arr[i][j] = m1->arr[i][j] - m2 -> arr[i][j] ;
+        }
+     }
+  }
   
 };
+int main()
+{
+    Matrix m1(2,2) ;
+    m1.acceptArr();
+
+    
+    Matrix m2(2,2);
+    m2.acceptArr();
+
+    Matrix m3(2,2);
+    m3.addArr(&m1,&m2) ;
+    m3.printArr();
+
+    m3.subArr(&m1,&m2);
+    m3.printArr(); 
+
+    
+    
+
+
+    return 0 ;
+}
